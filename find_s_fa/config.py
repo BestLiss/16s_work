74b2b3148e65a2e968d3dd872be100677670f465
BestLiss <53256470+BestLiss@users.noreply.github.com>
2023-10-23 17:38:41 +0800

    Delete find_s_fa directory

--- find_s_fa/config.py
+++
@@ -1,9 +0,0 @@
-
-
-threads = 40
-divide_count = 5
-work_path = 'select'
-
-# path = "C:\\Users\\lifu\\Desktop\\id.txt"
-# with open(path,'r',encoding='utf-8')as rf:
-#     print(len(rf.readlines()))
