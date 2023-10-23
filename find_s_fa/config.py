88265fa452e830dd652b87fd4c725042a4087593
BestLiss <53256470+BestLiss@users.noreply.github.com>
2023-10-23 17:37:45 +0800

    Add files via upload

---
+++ find_s_fa/config.py
@@ -0,0 +1,9 @@
+
+
+threads = 40
+divide_count = 5
+work_path = 'select'
+
+# path = "C:\\Users\\lifu\\Desktop\\id.txt"
+# with open(path,'r',encoding='utf-8')as rf:
+#     print(len(rf.readlines()))
